bc0a7800cfbdd82ed39227aba0c5e141b09ec30b
Pikafarad <dismagilov2001@gmail.com>
2024-04-02 19:19:41 +0500

    Убрал лишний QRandomGenerator, исправил интервал появления кнопки под частоту монитора, передал удаление кнопки при клике

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,27 +18,26 @@ MainWindow::MainWindow(QWidget *parent)
         [this, tmStart, rg_2]{
             tmStart->setInterval(rg_2->bounded(100, 1000));
             auto pb = new QPushButton("*", this);
-            QRandomGenerator *rg = QRandomGenerator::global();
             int squareWidth = 50;
             int squareHeight = 50;
-            int x = rg->bounded(0, this->width()- squareWidth);
-            int y = rg->bounded(0, 100);
+            int x = rg_2->bounded(0, this->width()- squareWidth);
+            int y = rg_2->bounded(0, 100);
             pb->setGeometry(x, y ,squareWidth, squareHeight);
             pb->show();
             auto tm = new QTimer(pb);
-            tm->setInterval(100);
-            int speed = rg->bounded(3,6);
+            tm->setInterval(17);
+            int speed = rg_2->bounded(1,2);
             connect(tm, &QTimer::timeout,
                 [pb, this, speed]{
                 pb->move(pb->pos()+QPoint(0,speed * (pb->underMouse() ? 2 : 1)));
-                connect(pb, &QPushButton::clicked, pb, &QPushButton::deleteLater);
+                if(pb->isDown())
+                    pb->deleteLater();
                 if(pb->pos().y() >= this->height()-pb->height())
                 {
                     QPalette Pal(palette());
                     Pal.setColor(QPalette::Background, Qt::red);
                     setAutoFillBackground(true);
                     setPalette(Pal);
-                    show();
                     setWindowTitle("LOSS");
                     pb->deleteLater();
                 }
@@ -47,7 +46,6 @@ MainWindow::MainWindow(QWidget *parent)
         });
         tmStart->start(rg_2->bounded(100, 1000));
 }
-
 
 MainWindow::~MainWindow()
 {
